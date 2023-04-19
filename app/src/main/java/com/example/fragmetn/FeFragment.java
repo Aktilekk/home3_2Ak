335507f67f047b7f3586f4162ba2b5864ba98349
Kaya <sergazievadil@gmail.com>
2023-04-19 18:46:40 +0600

    Initial commit

---
+++ app/src/main/java/com/example/fragmetn/FeFragment.java
@@ -0,0 +1,64 @@
+package com.example.fragmetn;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class FeFragment extends Fragment {
+
+    EditText editText;
+    EditText editTextTwo;
+    Button button;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fe, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews();
+        onClick();
+    }
+
+    private void initViews() {
+        editText = requireActivity().findViewById(R.id.name);
+        editTextTwo = requireActivity().findViewById(R.id.surname);
+        button = requireActivity().findViewById(R.id.buttonOne);
+    }
+
+    private void onClick() {
+        button.setOnClickListener(view -> {
+            if (!editText.getText().toString().isEmpty() && !editTextTwo.getText().toString().isEmpty()) {
+                dataSend();
+            } else {
+                Toast.makeText(getActivity(), "Введите ваше имя и фамилию", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void dataSend() {
+        String name = editText.getText().toString(),surname = editTextTwo.getText().toString();
+        Bundle bundle = new Bundle();
+        bundle.putString("name", name);
+        bundle.putString("surname", surname);
+        SecFragment secondFragment = new SecFragment();
+        secondFragment.setArguments(bundle);
+        FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container_view, secondFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+}
